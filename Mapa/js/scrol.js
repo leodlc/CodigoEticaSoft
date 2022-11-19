108bd9404737113481168ad90b419e1cb74e14ce
leodlc <ljde@espe.edu.ec>
2022-11-19 16:58:13 -0500

    Version 2.4

---
+++ Mapa/js/scrol.js
@@ -0,0 +1,11 @@
+let ubicacionPrincipal=window.pageYOffset;
+window.addEventListener("scroll",function(){
+    /* console.log(window.pagepageYOffset) */
+    let desplazamientoActual = window.pageYOffset; //100
+    if(ubicacionPrincipal >= desplazamientoActual){
+        document.getElementsByTagName("nav")[0].style.top = "0px" //200 > 180
+    }else{
+        document.getElementsByTagName("nav")[0].style.top = "-100px"
+    }
+    ubicacionPrincipal=desplazamientoActual;
+})
